a57e96b9006fced989a5ef7c2a9722501bcf6664
Prashanthi Ravichandran <prashanthi.ravichandran@gmail.com>
2020-04-04 11:20:29 -0400

    Updating preprocessing code

--- src/downloadData.R
+++ src/downloadData.R
@@ -20,41 +20,39 @@ aucDir <- dNames[2]
 rpkmDir <- dNames[3]
 
 pc.genes <- read.delim("/work-zfs/abattle4/parsana/networks_correction/data/etc/protein_coding.txt", 
-header = F, stringsAsFactors = F)
+                       header = F, stringsAsFactors = F)
 pc.genes <- pc.genes$V2[!pc.genes$V1 %in% c("chrM","chrY")]
 
 overlapping_genes <- read.delim("/work-zfs/abattle4/parsana/networks_correction/data/etc/ensembl_ids_overlapping_genes.txt",
-        stringsAsFactors = F)
+                                stringsAsFactors = F)
 
 
 studies_interest <- c("sra", "TCGA", "SRP012682")
 sapply(studies_interest, function(eachstudy,dirName){
-	download_study(eachstudy, outdir = paste(dirName,"/", eachstudy, sep = ""))
-	}, rawDir)
+  download_study(eachstudy, outdir = paste(dirName,"/", eachstudy, sep = ""))
+}, rawDir)
 
 sapply(studies_interest, function(eachstudy, pc_genes, ov_genes, dirName){
-        load(file.path(paste("raw/", eachstudy, sep = ""), "rse_gene.Rdata"))
-        rse_gene <- scale_counts(rse_gene, by = "auc", round = FALSE)
-        rse_gene <- select.genes(rse_gene, threshold = 0.1)
-        rse_gene <- rse_gene[rownames(rse_gene) %in% pc_genes,]
-        rse_gene <- rse_gene[!rownames(rse_gene) %in% ov_genes,]
-        counts <- SummarizedExperiment::assay(rse_gene, 1)
-        SummarizedExperiment::assay(rse_gene, 1) <- log2(counts+1)
-        saveRDS(rse_gene, file = paste(dirName, "/", eachstudy,".Rds", sep = ""))
-        }, pc.genes, overlapping_genes$x, aucDir)
+  load(file.path(paste("raw/", eachstudy, sep = ""), "rse_gene.Rdata"))
+  rse_gene <- scale_counts(rse_gene, by = "auc", round = FALSE)
+  rse_gene <- select.genes(rse_gene, threshold = 0.1)
+  rse_gene <- rse_gene[rownames(rse_gene) %in% pc_genes,]
+  rse_gene <- rse_gene[!rownames(rse_gene) %in% ov_genes,]
+  counts <- SummarizedExperiment::assay(rse_gene, 1)
+  SummarizedExperiment::assay(rse_gene, 1) <- log2(counts+1)
+  saveRDS(rse_gene, file = paste(dirName, "/", eachstudy,".Rds", sep = ""))
+}, pc.genes, overlapping_genes$x, aucDir)
 
 
 ## compute rpkm
 sapply(studies_interest, function(eachstudy, pc_genes, ov_genes, dirName){
-        load(file.path(paste("raw/", eachstudy, sep = ""), "rse_gene.Rdata"))
-        rse_rpkm <- getRPKM(rse_gene, length_var = "bp_length", mapped_var = NULL)
-	SummarizedExperiment::assay(rse_gene, 1) <- rse_rpkm
-        rse_gene <- select.genes(rse_gene, threshold = 0.1)
-        rse_gene <- rse_gene[rownames(rse_gene) %in% pc_genes,]
-        rse_gene <- rse_gene[!rownames(rse_gene) %in% ov_genes,]
-        counts <- SummarizedExperiment::assay(rse_gene, 1)
-        SummarizedExperiment::assay(rse_gene, 1) <- log2(counts+1)
-        saveRDS(rse_gene, file = paste(dirName, "/", eachstudy,".Rds", sep = ""))
-        }, pc.genes, overlapping_genes$x, rpkmDir)
-
-
+  load(file.path(paste("raw/", eachstudy, sep = ""), "rse_gene.Rdata"))
+  rse_rpkm <- getRPKM(rse_gene, length_var = "bp_length", mapped_var = NULL)
+  SummarizedExperiment::assay(rse_gene, 1) <- rse_rpkm
+  rse_gene <- select.genes(rse_gene, threshold = 0.1)
+  rse_gene <- rse_gene[rownames(rse_gene) %in% pc_genes,]
+  rse_gene <- rse_gene[!rownames(rse_gene) %in% ov_genes,]
+  counts <- SummarizedExperiment::assay(rse_gene, 1)
+  SummarizedExperiment::assay(rse_gene, 1) <- log2(counts+1)
+  saveRDS(rse_gene, file = paste(dirName, "/", eachstudy,".Rds", sep = ""))
+}, pc.genes, overlapping_genes$x, rpkmDir)
